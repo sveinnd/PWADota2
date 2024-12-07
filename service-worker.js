d08a3552d0b7d0cc495da764caf4a6945eb3be24
Sveinnd <sveinndpratama@gmail.com>
2024-12-07 09:38:45 +0700

    Initial commit

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'dota2-heroes-cache-v1';
+const urlsToCache = [
+    './',
+    './index.html',
+    './manifest.json',
+    'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
