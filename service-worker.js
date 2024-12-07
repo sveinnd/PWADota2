2d464e821786dac06b5f8520d6c11f5b19a27706
Sveinnd <sveinndpratama@gmail.com>
2024-12-07 13:17:04 +0700

    Update all files in the repository

--- service-worker.js
+++ service-worker.js
@@ -1,38 +1,33 @@
-const CACHE_NAME = 'dota2-heroes-cache-v1';
-const urlsToCache = [
-    './',
-    './index.html',
-    './manifest.json',
-    'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css'
-];
+const cacheName = "woilahhdotacik-v1"
+const preCache = ["/", "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css", "/indedx.html"]
 
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll(urlsToCache);
-        })
-    );
-});
+self.addEventListener("install", (e) => {
+  console.log("service worker installed")
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
-});
+  e.waitUntil(
+    (async () => {
+      const cache = await caches.open(cacheName)
+      cache.addAll(preCache)
+    })(),
+  )
+})
 
-self.addEventListener('activate', event => {
-    const cacheWhitelist = [CACHE_NAME];
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (!cacheWhitelist.includes(cacheName)) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
-});
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    (async () => {
+      const cache = await caches.open(cacheName)
+      const resCache = await cache.match(e.request)
+
+      if (resCache) return resCache
+
+      try {
+        const res = await fetch(e.request)
+
+        cache.put(e.request, res.clone())
+        return res
+      } catch (error) {
+        console.log(error)
+      }
+    })(),
+  )
+})
