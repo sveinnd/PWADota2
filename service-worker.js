13b30da40bcb9ca58bcdbab5e848a036b855a921
Sveinnd <55694460+sveinnd@users.noreply.github.com>
2025-07-01 16:34:40 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,5 @@
 const cacheName = "dota2heroes-v1";
 const preCache = [
-  "/",
   "/index.html",
   "/manifest.json",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
